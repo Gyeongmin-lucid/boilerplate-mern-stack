d42991bfa9392dce85fd48e13988281a6393160c
gyeongmin-lucid <kgm1306@pusan.ac.kr>
2021-05-23 19:31:06 +0900

    Add : Upload Page

---
+++ server/routes/product.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const {Product} = require('../models/Product');
+
+
+//=================================
+//             Product
+//=================================
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname )
+  }
+})
+
+var upload = multer({ storage: storage }).single("file")
+
+router.post("/image", (req, res) => {
+  // save the image from frontend
+  upload(req, res, err => {
+    if (err) {
+      console.log(err)
+      return res.json({ success: false, err})
+    }
+    return res.json({ 
+      success: true, 
+      filePath: res.req.file.path,
+      fileName : res.req.file.filename })
+  })
+})
+
+router.post("/", (req, res) => {
+  // put client info to DB
+  const product = new Product(req.body)
+  product.save( (err) => {
+    if(err) {
+      return res.status(400).json({
+      success: false,
+      err})
+    }
+    console.log("success request")
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+module.exports = router;
